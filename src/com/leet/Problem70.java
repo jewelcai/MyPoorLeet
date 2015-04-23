dba84c171f455eef9358a386ad0fb666956b353b
jewelcai <13121917815@153.com>
2015-04-23 10:48:58 +0800

    April 23th

---
+++ src/com/leet/Problem70.java
@@ -0,0 +1,17 @@
+package com.leet;
+
+public class Problem70 {
+	 public int climbStairs(int n) {
+	     if(n<=1)
+	    	 return n;
+		 int [] result= new int [n];
+	      result[0]=1 ;
+	      result[1]=2;
+	      for (int i = 2; i < result.length; i++) {
+			result[i]= result[i-1]+result[i-2];
+		}
+	        return result[n-1];
+	        
+	    }
+
+}
